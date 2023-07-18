e8d771deee7108708b2f5a427b16a5ec42aef3f7
stephanie sappho lenzo <sappho@sappho.io>
2023-07-18 03:29:27 -0400

    add function to automatically relaunch the game to enable the steam overlay

--- src/helpers/steam_helpers.cpp
+++ src/helpers/steam_helpers.cpp
@@ -3,11 +3,76 @@
 #ifdef CLIENT_DLL
 #include <helpers/steam_helpers.h>
 
+
+
+
+#ifdef _WIN32
+#include <icommandline.h>
+#include <Windows.h>
+void restartWithFixedCmdline()
+{
+    const char* cmdline = CommandLine()->GetCmdLine();
+    DevMsg(2, "%s\n", cmdline);
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Start the child process. 
+    if
+        (
+            !CreateProcess
+            (
+                NULL,				// No module name (use command line)
+                (LPSTR)cmdline,		// Command line
+                NULL,				// Process handle not inheritable
+                NULL,				// Thread handle not inheritable
+                FALSE,				// Set handle inheritance to FALSE
+                0,					// No creation flags
+                NULL,				// Use parent's environment block
+                NULL,				// Use parent's starting directory 
+                &si,				// Pointer to STARTUPINFO structure
+                &pi					// Pointer to PROCESS_INFORMATION structure
+            )
+            )
+    {
+        Warning("CreateProcess failed (%d).\n", GetLastError());
+        return;
+    }
+
+
+    exit(0);
+}
+
+
+
+void rmSourceTest()
+{
+    if (V_stristr(GetCommandLineA(), "sourcetest"))
+    {
+        CommandLine()->RemoveParm("-game sourcetest");
+        CommandLine()->AppendParm("-novid",         "");
+        CommandLine()->AppendParm("-multirun", "");
+
+        Sleep(1);
+        restartWithFixedCmdline();
+    }
+}
+#endif // WIN32
+
+
 // run our ctor
 CSteamHelpers g_SteamHelpers;
 CSteamHelpers::CSteamHelpers()
 {
     DevMsg(2, "CSteamHelpers CTOR->\n");
+
+    #ifdef CLIENT_DLL
+        rmSourceTest();
+    #endif
 
     // spin off a thread and wait until steam is up before we call any of our funcs
     SteamSpin = std::thread( &SpinUntilSteamIsAlive );
