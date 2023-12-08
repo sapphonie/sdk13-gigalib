26bc3f74da5c521ffc9227a54700fc68b6770e37
stephanie sappho lenzo <sappho@sappho.io>
2023-12-08 02:42:39 -0500

    add useless inc guard for windows libs

--- src/networking/networking.h
+++ src/networking/networking.h
@@ -5,8 +5,13 @@
 #endif
 
 
-
 #ifdef _WIN32
+
+
+#ifndef PROTOLIBS_DEFD_H
+#define PROTOLIBS_DEFD_H
+
+
 // hack because theres no macro for DEBUG that i can find
 // feel free to PR to add it to vpc if you know of one
 #ifdef DEBUG
@@ -24,6 +29,8 @@
     #pragma comment( lib, "../shared/sdk13-gigalib/bin/libprotobufd.lib" )
 #else
     #pragma comment( lib, "../shared/sdk13-gigalib/bin/libprotobuf.lib" )
+#endif
+
 #endif
 #endif
 
